29c4d248c7970627fe43b53a07a04669c4212350
mmann30 <michael.l.mann13@gmail.com>
2017-06-11 09:43:19 -0400

    add design notes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,2 +1,16 @@
 // Train Scheduler App FrontEnd JS
 
+// Will there be FrontEnd?
+
+
+// limit number of visible trains?
+// have option to delete trains?
+
+
+// rss feed of train schedule
+// rss alert of newly added train?
+
+// google map api?
+
+// destination limitations?
+// form selector box
